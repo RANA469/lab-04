8e51390f70a7ddb4ff4b966cab1d92e1ec54d9bf
RANA469 <ranashelbaya.98@yahoo.com>
2020-12-27 16:56:36 +0200

    initial add

---
+++ app/app.js
@@ -0,0 +1,27 @@
+
+var username=prompt('please insert your name');
+while (username === '' || username === null){
+    username = prompt('please insert your name');
+}
+var favmeal = prompt('please insert your favmeal');
+while (favmeal === '' || favmeal === null){
+    favmeal = prompt('please insert your favmeal');
+}
+alert('hello'+username+'. your favourite meal is'+favmeal)
+
+
+function displaycustomizedmessage(username,favmeal) {
+// declare the function 
+}
+alert('hello '+username+'your favourite meal is'+favmeal);
+
+function takeuserinput(name){
+    return prompt(name);
+}
+ function display_welcome_message(){
+     alert(me)
+ }
+
+
+
+
